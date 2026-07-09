e2ef651249e728eca28a51aa87cda7c4bf85496c
Giorgi Ka. <98465370+badur110@users.noreply.github.com>
2026-07-09 11:37:51 +0400

    Add hosting config example

---
+++ public_html/config.example.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'app_name' => 'ცხრამეტი ნაოჭი POS',
+    'restaurant_name' => 'ცხრამეტი ნაოჭი',
+    'restaurant_name_en' => 'Nineteen Pleats',
+    'restaurant_phone' => '598 01 83 83',
+    'restaurant_address' => '',
+    'thank_you_text' => 'გმადლობთ სტუმრობისთვის!',
+    'timezone' => 'Asia/Tbilisi',
+
+    'db_host' => 'localhost',
+    'db_name' => 'CHANGE_DATABASE_NAME',
+    'db_user' => 'CHANGE_DATABASE_USER',
+    'db_pass' => 'CHANGE_DATABASE_PASSWORD',
+    'db_charset' => 'utf8mb4',
+
+    'cancel_password' => 'cancel123',
+];
