1d7e67daa67a5dbe05f1b8b4a244c91124f46cda
Giorgi Ka. <98465370+badur110@users.noreply.github.com>
2026-07-30 11:58:52 +0400

    Refresh direct printing script version

--- public_html/assets/close-confirm-loader.js
+++ public_html/assets/close-confirm-loader.js
@@ -1,4 +1,4 @@
 (function () {
   document.write('<script src="/assets/close-confirm.js?core=1&v=22"><\/script>');
-  document.write('<script src="/assets/direct-print.js?v=1"><\/script>');
+  document.write('<script src="/assets/direct-print.js?v=2"><\/script>');
 })();
