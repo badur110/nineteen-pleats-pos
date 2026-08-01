c15978d38a25df69802b85d75db9498dfb1377aa
Giorgi Ka. <98465370+badur110@users.noreply.github.com>
2026-08-01 16:24:35 +0400

    Load twelve-table layout and synchronization

--- public_html/assets/close-confirm-loader.js
+++ public_html/assets/close-confirm-loader.js
@@ -3,4 +3,5 @@
   document.write('<script src="/assets/direct-print.js?v=2"><\/script>');
   document.write('<script src="/assets/cash-movement-polish.js?v=1"><\/script>');
   document.write('<script src="/assets/pwa-install.js?v=2"><\/script>');
+  document.write('<script src="/assets/tables-12.js?v=1"><\/script>');
 })();
