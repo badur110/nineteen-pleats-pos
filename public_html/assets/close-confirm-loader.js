32703d5188c4425022c0b690d1899f1da6447413
Giorgi Ka. <98465370+badur110@users.noreply.github.com>
2026-08-03 13:19:43 +0400

    Load latest table layout fixes

--- public_html/assets/close-confirm-loader.js
+++ public_html/assets/close-confirm-loader.js
@@ -3,7 +3,7 @@
   document.write('<script src="/assets/direct-print.js?v=2"><\/script>');
   document.write('<script src="/assets/cash-movement-polish.js?v=1"><\/script>');
   document.write('<script src="/assets/pwa-install.js?v=2"><\/script>');
-  document.write('<script src="/assets/tables-12.js?v=3"><\/script>');
+  document.write('<script src="/assets/tables-12.js?v=4"><\/script>');
   document.write('<script src="/assets/table-cancel.js?v=1"><\/script>');
-  document.write('<script src="/assets/table-page-flow.js?v=1"><\/script>');
+  document.write('<script src="/assets/table-page-flow.js?v=2"><\/script>');
 })();
